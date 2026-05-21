876732a71ec0463d4c193510c57127d46db98102
Madan Pokhrel <madhanpokhrel9@gmail.com>
2026-05-21 08:31:17 +0545

    auth handled with firebase

---
+++ lib/core/services/auth_service.dart
@@ -0,0 +1,19 @@
+import "package:firebase_auth/firebase_auth.dart";
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Stream<User?> get authStateChanges => _auth.authStateChanges();
+  User? get currentUser => _auth.currentUser;
+
+  Future<void> signIn(String email, String password) async {
+    await _auth.signInWithEmailAndPassword(email: email, password: password);
+  }
+
+  Future<void> signUp(String email, String password) async {
+    await _auth.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+  }
+}
---
+++ lib/ui/screens/auth/auth_screen.dart
@@ -0,0 +1,86 @@
+import "package:flutter/material.dart";
+import "package:quizapp/core/services/auth_service.dart";
+
+class AuthScreen extends StatefulWidget {
+  const AuthScreen({super.key});
+
+  @override
+  State<AuthScreen> createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  final _email = TextEditingController();
+  final _password = TextEditingController();
+
+  final _authService = AuthService();
+
+  bool isLogin = true;
+  bool isLoading = false;
+
+  void singUp() async {
+    setState(() {
+      isLoading = true;
+    });
+    try {
+      if (isLogin) {
+        await _authService.signIn(_email.text.trim(), _password.text.trim());
+        ScaffoldMessenger.of(
+          context,
+        ).showSnackBar(SnackBar(content: Text("Login successfull")));
+      } else {
+        await _authService.signUp(_email.text.trim(), _password.text.trim());
+      }
+    } catch (e) {
+      if (mounted) {
+        ScaffoldMessenger.of(
+          context,
+        ).showSnackBar(SnackBar(content: Text(e.toString())));
+      }
+    }
+    if (mounted) setState(() => isLoading = false);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Auth Screen Login/Signup")),
+      body: Padding(
+        padding: EdgeInsetsGeometry.all(24.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _email,
+              decoration: const InputDecoration(labelText: "Email"),
+            ),
+            SizedBox(height: 20),
+            TextField(
+              controller: _password,
+              decoration: InputDecoration(labelText: "Password"),
+              obscureText: true,
+            ),
+            SizedBox(height: 20),
+            isLoading
+                ? const CircularProgressIndicator()
+                : ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: const Size.fromHeight(50),
+                    ),
+                    onPressed: singUp,
+                    child: Text(
+                      isLogin ? 'Login' : 'Sign Up',
+                      style: const TextStyle(fontSize: 18),
+                    ),
+                  ),
+
+            TextButton(
+              onPressed: () => setState(() => isLogin = !isLogin),
+              child: Text(
+                isLogin ? 'Create an account' : 'I already have an account',
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
