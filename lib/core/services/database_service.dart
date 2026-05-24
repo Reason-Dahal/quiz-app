6070893b7f6e6043cfc4339f92cfa67d5cf50957
Madan Pokhrel <madhanpokhrel9@gmail.com>
2026-05-24 08:43:23 +0545

    categories database added

---
+++ lib/core/services/database_service.dart
@@ -0,0 +1,26 @@
+import "package:cloud_firestore/cloud_firestore.dart";
+import "package:quizapp/data/models/question_model.dart";
+
+class DatabaseService {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  Future<List<String>> getCategories() async {
+    final snapshot = await _firestore.collection('categories').get();
+    print("respnse: $snapshot");
+    final result = snapshot.docs.map((doc) => doc['name'] as String).toList();
+    print("data: $result");
+    return result;
+  }
+
+  Future<List<QuestionModel>> getQuestionsByCategory(String category) async {
+    final snapshot = await _firestore
+        .collection('categories')
+        .doc(category)
+        .collection('questions')
+        .get();
+
+    return snapshot.docs
+        .map((doc) => QuestionModel.fromJson(doc.data()))
+        .toList();
+  }
+}
--- lib/ui/screens/home/home_screen.dart
+++ lib/ui/screens/home/home_screen.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:quizapp/core/services/auth_service.dart';
+import 'package:quizapp/core/services/database_service.dart';
 
 class HomeScreen extends StatelessWidget {
   HomeScreen({super.key});
 
   final AuthService _authService = AuthService();
+  final DatabaseService _databaseService = DatabaseService();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Home Screen"),
+        title: const Text("Choose Category"),
         actions: [
           IconButton(
             onPressed: () async {
@@ -21,7 +23,58 @@ class HomeScreen extends StatelessWidget {
         ],
       ),
 
-      body: const Center(child: Text("Home Screen")),
+      body: FutureBuilder<List<String>>(
+        future: _databaseService.getCategories(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          if (snapshot.hasError) {
+            return const Center(child: Text('Error loading categories'));
+          }
+
+          final categories = snapshot.data ?? [];
+
+          if (categories.isEmpty) {
+            return Center(
+              child: Text(
+                "No Categories. Please enter the categories on the database.",
+              ),
+            );
+          }
+
+          return ListView.builder(
+            padding: const EdgeInsets.all(16),
+            itemCount: categories.length,
+            itemBuilder: (context, index) {
+              final category = categories[index];
+              return Card(
+                elevation: 2,
+                margin: const EdgeInsets.only(bottom: 16),
+                child: ListTile(
+                  contentPadding: const EdgeInsets.all(16),
+                  title: Text(
+                    category,
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  trailing: const Icon(
+                    Icons.play_circle_fill,
+                    color: Colors.deepPurple,
+                    size: 36,
+                  ),
+                  // onTap: () {
+                  //   Navigator.pushNamed(context, '/quiz', arguments: category);
+                  // },
+                ),
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
